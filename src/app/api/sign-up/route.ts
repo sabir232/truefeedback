e878802bd6f1cadfa131ee60b512760654dae0b8
sabir232 <sabirkhan98563@gmail.com>
2024-06-24 09:52:59 +0530

    Created resend email for email sending for verification and created the api response and created api route and continue the route creation

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,26 @@
+import dbConnect from "@/lib/dbConnect";
+
+import UserModel from "@/model/User";
+
+import bcrypt from "bcryptjs";
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+
+
+export async function POST(request: Request) {
+    await dbConnect()
+    try { 
+        const { username, email, password } = await request.json()
+        
+    } catch (error) {
+        console.log("Error registering users",error);
+        return Response.json({
+            success: false,
+            message: "Error registering users",
+        }, {
+            status:500
+        })
+    }
+}
+
+
+
