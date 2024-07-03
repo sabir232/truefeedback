b3456ca098989df7a7357cabe5556dd4c023dfc5
sabir232 <sabirkhan98563@gmail.com>
2024-07-03 09:55:34 +0530

    implemented user dashboard

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -1,7 +1,7 @@
 import { Message } from "@/model/User";
-export interface ApiResponse{
-    success: boolean;
-    message: string;
-    isAccepectingMessages?:boolean
-    messages?:Array<Message>
+export interface ApiResponse {
+  success: boolean;
+  message: string;
+  isAccepectingMessages?: boolean;
+  messages?: Array<Message>;
 }
